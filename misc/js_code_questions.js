0401f0fc6185789d6a511180121ec5b5d14d2c74
willashley23 <willashley23@gmail.com>
2017-07-05 20:11:13 -0700

    first

---
+++ misc/js_code_questions.js
@@ -0,0 +1,298 @@
+/*jshint esversion: 6 */
+
+// Permutations
+
+var permArr = [],
+  usedChars = [];
+
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length === 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr;
+}
+
+function subs (arr) {
+
+  var i, j, temp;
+  var result = [];
+  var arrLen = arr.length;
+  var power = Math.pow;
+  var permutations = power(2, arrLen);
+
+  for (i = 0; i < permutations ; i++) {
+    temp = '';
+    for (j = 0; j < arrLen; j++) {
+      if ((i & power(2, j))) {
+        temp += arr[j];
+        }
+    }
+    if (temp !== '') {
+      result.push(temp);
+    }
+  }
+  return result.join('\n');
+}
+
+
+function isWordBreakable (s, dict, answer) {
+  // console.log(s + '  ' + answer);
+  var strLen = s.length;
+  if (strLen === 0) {
+    console.log(answer);
+    return true;
+  } else {
+    var prefix = '';
+    for (var i = 0; i < strLen; i++) {
+      // add one char at a time
+      prefix += s.charAt(i);
+      // check if prefix exists in dictionary
+      // if (dict.includes(prefix)) { // Array.prototype.includes() is an ES7 Feature
+      if (dict.indexOf(prefix) > -1) {
+        //add prefix to the answer and make a recursive call
+        answer += prefix + ' ';
+        var suffix = s.slice(i + 1);
+        if (isWordBreakable(suffix, dict, answer)) {
+          return true;
+        }
+      } 
+      //console.log(prefix + '  backtrack');
+    }
+  }
+}
+
+
+
+// Valid balanced parens O(n)
+
+function parenthesesAreBalanced(str) {
+  let parens = "{}[]()";
+  let stack = [];
+  let character;
+  let position;
+  
+  for(let i = 0; character = str[i]; i++ ) {
+    position = parens.indexOf(character);
+    
+    if (position === -1) {
+      continue;
+    }
+    
+    if (position % 2 === 0) {
+      stack.push(position + 1);
+    } else {
+      if (stack.length === 0 || stack.pop() != position) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+// Stock picker in O(n) time and O(1) space.
+function getMaxProfit(stockPricesYesterday) {
+  
+    if (stockPricesYesterday.length < 2) {
+        return 0;
+    }
+    
+    let minPrice = stockPricesYesterday[0];
+    let maxProfit = stockPricesYesterday[1] - stockPricesYesterday[0];
+
+    for (let i = 1; i < stockPricesYesterday.length; i++) {
+        let currentPrice = stockPricesYesterday[i];
+        let potentialProfit = currentPrice - minPrice;
+        maxProfit = Math.max(maxProfit, potentialProfit);
+        minPrice = Math.min(minPrice, currentPrice);
+    }
+    return maxProfit;
+}
+
+function rearrange(str) {
+  let freq = {};
+  let strarr = str.split("");
+  let answer = new Array(str.length);
+  for (let i = 0; i < str.length; i++) {
+    if (freq[str[i]]) {
+      freq[str[i]] += 1;
+    } else {
+      freq[str[i]] = 1;
+    }
+  }
+  
+  for (let el in freq) {
+    if (freq[el] >= (str.length / 2) + 1) {
+      return "No valid solution";
+    }
+  }
+  
+  let k = 0;
+  for (let j = 0; j < answer.length; j += 2) {
+    answer[j] = strarr[k];
+    strarr.shift();
+  }
+  for (let n = 1; n < answer.length; n += 2) {
+    answer[n] = strarr[k]
+    strarr.shift();
+  }
+  return (answer.join(""))
+}
+
+// Was looking for a good example of a Fisher–Yates shuffle
+function shuffle(array) {
+  let m = array.length;
+  let t;
+  let i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+
+// Reverse Doubly Linked List
+
+function reverseDoublyLL(dll){
+   var head = dll.head,
+       current = dll.head,
+       tmp;
+   while(current){
+      tmp = current.next;
+      current.next = current.previous;
+      current.previous = tmp;
+      if(!tmp){
+         //set the last node as header
+         dll.head = current;
+      }
+      current = tmp;
+   }
+  return dll;
+}
+   
+// Delete Kth node from end
+
+
+function deleteKthFromEnd(sll, k){
+   var node = sll.head,
+       i = 1,
+       kthNode,
+       previous;
+   if(k<=0) return sll;
+
+    while(node){
+      if(i == k) kthNode = sll.head;
+      else if(i-k>0){
+       previous = kthNode;
+       kthNode = kthNode.next;
+      }
+      i++;
+
+      node = node.next;
+    }
+    //kth node is the head
+    if(!previous)
+       sll.head = sll.head.next;
+    else{
+     previous.next = kthNode.next;
+   }
+   return sll;
+}
+
+
+// Delete Node from DLL
+
+function deleteNodeDLL(val){
+   var current = dll.head,
+       previous;
+   
+   //delete head
+   if(current.value == val){
+      dll.head = current.next;
+      //if there is only one node, then dll.head is null
+      if(dll.head) dll.head.previous = null;
+      return dll;
+   }
+
+   while(current.next){
+      if(current.value == val){
+         previous.next = current.next;
+         current.next.previous = previous;
+         return dll;
+     }
+     previous = current;
+     current = current.next;
+   }
+   
+   //delete last node
+   if(current.value == val){
+     previous.next = null;
+   }
+   return dll;
+}
+
+// Detect Loop in SLL
+
+function detectLoop(sll){
+   var slowPointer = sll.head, 
+       fastPointer = sll.head;
+
+   while(slowPointer && fastPointer && fastPointer.next){
+     slowPointer = slowPointer.next;
+     fastPointer = fastPointer.next.next;
+
+     if(slowPointer == fastPointer){
+        return true;
+     }
+   }
+   return false;
+}
+
+// Insert to Sorted LL
+
+function pushSorted(sll, val){
+   var head = sll.head,
+       current = head,
+       previous;
+   //value lower than head value
+   if(val < sll.head.value){
+      sll.head = {value: val, next: head};
+      return sll;
+   }
+
+   while(current){
+      if(current.value > val){
+         previous.next = {value: val, next: current};
+         return sll;
+      }
+      previous = current;
+      current = current.next;
+   } 
+   //value higher than the last node value
+   previous.next = {value:val, next: null};
+   return sll;
+}
+
+// Number of bits in a num
+
+function bitSize(num) {
+    return num.toString(2).length;
+}
+
+
---
+++ recursion/js_recursion.js
@@ -0,0 +1,174 @@
+function range(n,k) {
+  if (k - n === 2) {
+    return [n+1];
+  }
+  let coll = range(n, k-1);
+  coll.push(k-1);
+  return coll;
+  
+}
+
+// Write a recrusive function that takes an accumulator and any number of ints following
+// and return the accumulator which should be the sum of all the ints. 
+function f(x, ...y) {
+  if (!y.length) {
+    return x;
+  } 
+  else {
+    x += y.shift();
+    return f(x, ...y);
+  }
+}
+
+f(10,2,4,7,8,4);
+// To solve, simply use the ... rest operator. 
+
+function inject(a) {
+  if (a.length === 1) {
+    return a[0];
+  } else {
+    return a.pop() + inject(a);
+  }
+}
+
+function gcd(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    
+    // If the order is wrong, swap them
+    if (b > a) {
+        var temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    while (true) {
+        if (b === 0) {
+            return a;
+        }
+        
+        a %= b;
+        
+        if (a === 0) {
+            return b;
+        }
+        
+        b %= a;
+    }
+} 
+
+function lcm(a,b) {
+    return  (a * b) / gcd(a,b);
+}
+
+
+function fibs(n) {
+  if (n === 1) {
+    return [0, 1];
+  }
+  fibsArray = fibs(n-1);
+  fibsArray.push(fibsArray[fibsArray.length-1] + fibsArray[fibsArray.length-2]);
+  return fibsArray;
+}
+
+
+function nPrimes(n) {
+    let primes = [];
+    for(let i = 3; primes.length < n-1; i += 2) {
+        if (primes.every(prime => {return i % prime !== 0})) {
+            primes.push(i);
+        }
+    }
+    primes.unshift(2);
+    console.log(primes);
+    return primes;
+}
+
+function bsearch(array, target) {
+  let midPoint = array[Math.floor(array.length / 2)];
+  if (midPoint == target) {
+    return true;
+  }
+  
+  if (array.length <= 1) {
+    return false;
+  }
+  // search RHS
+  if (target > midPoint) {
+    let truncatedArray = array.slice(array.indexOf(midPoint), array.length);
+    return bsearch(truncatedArray, target);
+  } 
+  // search LHS
+  else if (target < midPoint) {
+    let truncatedArray = array.slice(0, array.indexOf(midPoint));
+    return bsearch(truncatedArray, target);
+  }
+  else {
+    return midPoint;
+  }
+}
+
+
+let b = [1,2,4,6,9,11,19,22,34,55,67,99,104];
+bsearch(b, 67);
+
+function exponent(n,k) {
+  if (k <= 1) {
+    return n;
+  }
+  if (k === 0) {
+    return 1;
+  }
+  return (n * exponent(n, k-1));
+}
+
+Array.prototype.br_search = function (target)   
+{  
+  var half = parseInt(this.length / 2);  
+  if (target === this[half])   
+  {  
+    return half;  
+  }  
+  if (target > this[half])   
+  {  
+    return half + this.slice(half,this.length).br_search(target);  
+  }   
+  else  
+  {  
+    return this.slice(0, half).br_search(target);  
+  }  
+};  
+
+Array.prototype.merge_Sort = function () {  
+  if (this.length <= 1)   
+  {  
+    return this;  
+  }  
+  
+  var half = parseInt(this.length / 2);  
+  var left = this.slice(0, half).merge_Sort();  
+  var right = this.slice(half,     this.length).merge_Sort();  
+  var merge = function (left, right)   
+  {  
+    var merged = [];  
+
+      while (left.length > 0 && right.length > 0)  
+      {  
+          merged.push((left[0] <= right[0]) ? left.shift() : right.shift());  
+      }  
+
+      return merged.concat(left).concat(right);  
+  };  
+  
+  return merge(left, right);  
+};  
+  
+var a = [34,7,23,32,5,62];  
+console.log(a.merge_Sort()); 
+  
+l= [0,1,2,3,4,5,6];  
+
+console.log(range(2,9))
+console.log(inject([1,2,3]))
+console.log(fibs(20))
+console.log(exponent(2,3))
